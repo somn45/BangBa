e94828505f39072f802ba7e9f106b69e56f6875d
somn45 <somnsomn45@gmail.com>
2022-12-21 18:16:24 +0900

    fix : 서버에서 포트 번호가 보이도록 수정

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import app from './server';
 
 const PORT = 4000;
 
-app.listen(PORT, () => console.log('✅ Server connect success.'));
+app.listen(PORT, () => console.log(`✅ Server connect success. PORT : ${PORT}`));
